a7d11392ecf27a87bcac0a726d82c0f3d469e868
Eugenia Galkina <yavsehmilee@gmail.com>
2022-07-02 20:08:19 +0300

    the 1st array created and printed

---
+++ task/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либа равна трем символам. 
+
+void FillArray(string [] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i]=Console.ReadLine() ?? "0";
+    }
+}
+void PrintArray (string [] arr)
+{
+    for (int i=0; i<arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+Console.WriteLine("введите элементы массива:");
+string[] array = new string[5];
+FillArray(array);
+Console.WriteLine("ваш массив:");
+PrintArray(array);
+Console.WriteLine();
