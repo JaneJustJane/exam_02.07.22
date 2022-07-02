aba25ac9ed7a3b328e5287f758676bced14372be
Eugenia Galkina <yavsehmilee@gmail.com>
2022-07-02 20:16:05 +0300

    new array filled and printed

--- task/Program.cs
+++ task/Program.cs
@@ -30,3 +30,18 @@ for (int i=0; i<array.Length; i++)
     if (array[i].Length <=3)
         index=index+1;  
 }
+
+string[] array1 = new string[index];
+for (int i=0; i<array.Length; i++)
+{
+    if (array[i].Length <=3)
+    {
+        array1[j]=array[i];
+        j=j+1;
+    } 
+}
+Console.WriteLine("новый массив:");
+for (int i=0; i<array1.Length; i++)
+    {
+        Console.Write($"{array1[i]} ");
+    }
